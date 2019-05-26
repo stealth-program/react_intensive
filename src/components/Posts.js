90a4635d1018c583642f9f669c880a146da8d9a6
Evgeny <st054504@student.spbu.ru>
2019-05-26 22:50:05 +0300

    app

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,72 @@
 import React, {Component} from 'react';
-import Post from './Post';
+import User from './User';
+import Instaservice from '../services/instaservice';
+import ErrorMessage from './ErrorMessage';
 
 export default class Posts extends Component {
+    Instaservice = new Instaservice();
+    state = {
+        posts: [],
+        error: false
+    };
+
+    componentDidMount() {
+        this.updatePosts();
+    }
+
+    updatePosts() {
+        this.Instaservice.getAllPosts()
+            .then(this.onPostsLoaded)
+            .catch(this.onError);
+    }
+
+    onPostsLoaded = (posts) => {
+        this.setState({
+            posts: posts,
+            error: false
+        })
+    }
+
+    onError = (err) => {
+        this.setState({
+            error: true
+        })
+    }
+
+    renderItems(arr) {
+        return arr.map(item => {
+            const {name, altname, photo, src, alt, descr, id} = item;
+
+            return (
+                <div key={id} className="post">
+                    <User
+                        src={photo}
+                        alt={altname}
+                        name={name}
+                        min/>
+                    <img src={src} alt={alt}/>
+                    <div className="post__name">
+                        {name}
+                    </div>
+                    <div className="post__descr">
+                        {descr}
+                    </div>
+                </div>
+            )
+        })
+    }
+
     render() {
+        const {error, posts} = this.state;
+        if (error) {
+            return <ErrorMessage/>
+        }
+
+        const items = this.renderItems(posts);
+
         return (
             <div className="left">
-                <Post alt="nature" src="https://itproger.com/img/courses/1523436361.jpg"/>
-                <Post alt="nature" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Shaqi_jrvej.jpg/1200px-Shaqi_jrvej.jpg"/>
+                {items}
             </div>
 
         )
